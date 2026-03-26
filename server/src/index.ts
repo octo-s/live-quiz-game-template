ed3e0d8fe7364eb4ae6b43c5e5e9f7f546c7a4ea
tatyana.lebedeva <tslebedeva3103@gmail.com>
2026-03-26 12:34:15 +0100

    start: start task

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,13 @@
-import { WebSocketServer } from 'ws';
-
+import { WebSocketServer, WebSocket } from 'ws';
+import type { User, Game } from './types';
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // WebSocket server
 const wss = new WebSocketServer({ port: PORT });
+
+const users = new Map<string, User>();
+const games = new Map<string, Game>();
+const wsToUser = new Map<WebSocket, string>();
+
+console.log(`WebSocket server started on ws://localhost:${PORT}`);
